4f631af2810e0df1fd5bdd4ae146f958bbe5442a
AndreaGenovese03 <118006599+AndreaGenovese03@users.noreply.github.com>
2022-12-08 14:15:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,7 @@
 #include "lib.h"
 
  bool funzione(char &a){
-    if ((a>=65 && a<=90)||(a>=97 && a<=122)) {
+    if ((a>=65 and a<=90)or(a>=97 and a<=122)) {
         if (a<=90){
             a+=32;
         }else{
