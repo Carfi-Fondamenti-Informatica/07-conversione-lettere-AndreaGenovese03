42d3e15dedaba01ee95622c9afbb0c9de10e486c
AndreaGenovese03 <118006599+AndreaGenovese03@users.noreply.github.com>
2022-12-08 14:21:17 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,12 +1,13 @@
 #include "lib.h"
 
  bool funzione(char &a){
-    if (a>=65 and a<=90) {
-            a+=32;
+    if ((a>=65) and (a<=90)) {
+            a=(a+32);
             return true;
-        }else if(a>=97 and a<=122){
-            a-=32;
+        }else if((a>=97) and (a<=122)){
+            a=(a-32);
             return true;
+        }else{
+            return false;
         }
-        return false;
 }
