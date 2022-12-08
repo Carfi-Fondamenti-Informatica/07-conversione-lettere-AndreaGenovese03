8fd1f4430caf5d74ef68e2a5cca8bfde88ddea9d
AndreaGenovese03 <118006599+AndreaGenovese03@users.noreply.github.com>
2022-12-08 13:53:05 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include <lib.h>
+ bool controlloCovertoCarattere (char &a){
+    if ((a>=65 && a<=90)||(a>=97 && a<=122)) {
+        if (a<=90)
+            a=a+32;
+        else
+            a=a-32;
+        return true;
+    }
+    else
+        return false;
+}
